548e7121254ee899743de389d687b691123d9bc4
theaswin <aswinnathofficial@gmail.com>
2023-11-28 11:10:16 +0530

    Neccessary data base functionalities are created

--- DBHadler.py
+++ DBHadler.py
@@ -2,19 +2,114 @@
 This perticular file Handle our database functionalities.Such as creating database,
 creation of new table for input data,updating data,adding new sensor
 '''
+import sqlite3
+from utils import UtilsMaster
+import csv
+import pandas as pd
+import os
 
-class dbHandlerMaster():
+class dbHandlerMaster(UtilsMaster):
     def __init__(self):
-        pass
+        self.newTableForSensor = 'Sensor2'
+        self.DataBaseName = 'Sensor'
+        self.csvFile = 'PYgenerated/data.csv'
+        self.output_csv = 'DBExtracted/output.csv'
 
-    def createNewDatabase(self):
-        pass
+    def connectWithDataBase(self):
+        try:
+            conn  = sqlite3.connect(self.DataBaseName+'.db')
+        except ConnectionError:
+            raise ConnectionError()
+        return conn
+        
+
+    def createNewTable(self):
+        try:
+            conn  = self.connectWithDataBase()
+            conn.execute('''
+                CREATE TABLE IF NOT EXISTS SensorData (
+                         date TEXT,
+                         DATA INT
+                )
+                         ''')
+            # conn.execute("Drop table SensorData")
+            print("Created")
+            conn.commit()
+            conn.close()
+        except:
+            print("failed")
+
 
     def insertDataIntoTable(self):
-        pass
+        conn = self.connectWithDataBase()
+        
+        with open(self.csvFile,'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            next(csv_reader,None)
+            
+            for row in csv_reader:
+                conn.execute('''
+                INSERT INTO SensorData (DATA,date) VALUES (?,?)
+                    ''',(row[0],row[1]))
+            conn.commit()
+            conn.close()
+        
+
+
+
+    def ExtractingDataFromDB(self):
+        self.fileFromDb = 'DBExtracted/'
+        self.fileName = 'output.csv'
+        try:
+            conn = self.connectWithDataBase()
+            sqlSelect = "SELECT *  FROM SensorData"
+            cursor = conn.cursor()
+
+            cursor.execute(sqlSelect)
+
+            results = cursor.fetchall()
+
+            #Get the headers of the csv file
+            headers = [i[0] for i in cursor.description]
+
+            #Make the CSV ouput directory
+            if not os.path.isdir(self.fileFromDb):
+                os.makedirs(self.fileFromDb)
+
+            # Open CSV file for writing.
+            csvFile = csv.writer(open(self.fileFromDb + self.fileName, 'w', newline=''),delimiter=',', lineterminator='\r\n',quoting=csv.QUOTE_ALL, escapechar='\\')
+
+            # Add the headers and data to the CSV file.
+            csvFile.writerow(headers)
+            csvFile.writerows(results)
+
+
+        except Exception as e:
+            print("not extracted")
+        
+
 
     def updateExistingTable(self):
-        pass
+        conn = self.connectWithDataBase()
+        cursor = conn.cursor()
+        new_date = 88
+        data = 4
+        cursor.execute('''
+            UPDATE SensorData
+                SET date = ?
+                WHERE DATA = ?
+                ''',(new_date,data))
+        conn.commit()
+        conn.close()
 
-    def CreateNewTableForSensor(self):
-        pass
+    def CreateNewTableForNewSensor(self):
+        conn  = self.connectWithDataBase()
+        cursor = conn.cursor()
+
+
+
+# v = dbHandlerMaster()
+# v.createNewTable()
+# v.insertDataIntoTable()
+# v.updateExistingTable()
+# v.ExtractingDataFromDB()
--- utils.py
+++ utils.py
@@ -1,14 +1,31 @@
 '''
 Utility file
 '''
+import random
+from datetime import datetime
+import csv
 
 class UtilsMaster():
     def __init__(self) -> None:
-        pass
+        self.dataset = []
+        self.dates = []
+        self.current_datetime = datetime.now()
 
-    def GenerateRandomData(self):
-        pass
+    def GenerateRandomDataAndDate(self):
+        for i in range(1,10):
+            self.formatted_datetime = self.current_datetime.strftime("%Y-%m-%d")
+            self.randomData = random.randint(0,9)
+            self.dataset.append(self.randomData)
+            self.dates.append(self.formatted_datetime)
     
+    def DataToCSV(self):
+        rows = zip(self.dataset,self.dates)
+        with open('PYgenerated/data.csv', 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(['DATA', 'date'])
+            csv_writer.writerows(rows)
+    
+
     def GetManualInput(self):
         pass
     
@@ -20,3 +37,8 @@ class UtilsMaster():
 
     def UpdateExistingSensorData(self):
         pass
+
+
+v = UtilsMaster()
+v.GenerateRandomDataAndDate()
+v.DataToCSV()
